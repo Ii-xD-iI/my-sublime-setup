9cea7050d8fdf2b00e414a7ba664cf48ef1abe0b
The xD <70330230+Ii-xD-iI@users.noreply.github.com>
2021-04-07 04:23:07 +0530

    +file io

---
+++ template.cpp
@@ -0,0 +1,36 @@
+#include "bits/stdc++.h"
+
+#define len(Z) (int)(Z).size()
+#define elif else if
+#define all(Z) (Z).begin(), (Z).end()
+#define ceil(X,Y) (X + Y-1)/Y
+#define test(A) int A;cin>>A;while(A-->0)
+
+using namespace std;
+
+const int MAXN=4e3+7, INF = 2e9+7, MOD = 1e9+7;//check the limits u dum dum
+const long long INFLL = 2e18+7;
+
+void IO(string Bessie);
+
+void Solve(){
+
+}
+
+int32_t main() {
+	IO("");
+    	
+	test(Dolphin_OTZ)
+    	Solve();
+    return 0;
+}
+
+void IO(string Bessie = "") {   
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);cout.tie(nullptr);
+    if (len(Bessie)) {
+        freopen((Bessie+".txt").c_str(), "r", stdin);
+        freopen((Bessie+".txt").c_str(), "w", stdout);
+    } 
+}
+// taskkill /IM "template.exe" /F
