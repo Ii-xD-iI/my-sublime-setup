7ad7b4c1e1d3232f9a11194d42442bfe6866ba9a
The xD <70330230+Ii-xD-iI@users.noreply.github.com>
2021-05-06 17:27:20 +0530

    smug

--- template.cpp
+++ template.cpp
@@ -1,28 +1,15 @@
+using namespace std;
 #include "bits/stdc++.h"
-
 #define len(Z) (int)(Z).size()
 #define elif else if
 #define all(Z) (Z).begin(), (Z).end()
 #define ceil(X,Y) (X + Y-1)/Y
-#define test(A) int A;cin>>A;while(A-->0)
-
-using namespace std;
-
-const int MAXN=4e3+7, INF = 2e9+7, MOD = 1e9+7;//check the limits u dum dum
-const long long INFLL = 2e18+7;
-
 void IO(string Bessie);
-
-void Solve(){
-
-}
 
 int32_t main() {
 	IO("");
-    	
-	//test(Dolphin_OTZ)
-    	Solve();
-    return 0;
+
+	return 0;
 }
 
 void IO(string Bessie = "") {   
